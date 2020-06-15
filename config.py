5122ff186e82e10dae9bc3b78e4ea675e03c6b93
Enegg <42005170+Enegg@users.noreply.github.com>
2020-06-15 21:41:16 +0200

    Removed unecessary config entries

--- config.py
+++ config.py
@@ -9,65 +9,6 @@ purge_confirm_emote = '<:kamehameha:704775089044062278>'
 #say
 abbreviations = {'WU': 'Workshop Unlimited', 'M': 'Matrix', 'WB': 'W&BS', 'AC': 'Axis Convergence'}
 #stats
-link = 'https://workshopunlimited.herokuapp.com/api/items/'
-trans_range = ['⚪','🔵','🟣','🟠','🟤','⚪']
-tiers = ['COMMON','RARE','EPIC','LEGENDARY','MYTHICAL','DIVINE']
-sprite_path = 'https://killin-sm.github.io/wudev/img/items/'
-colors = {'EXPLOSIVE': 0xb71010, 'ELECTRIC': 0x106ed8, 'PHYSICAL': 0xffb800}
-WUabbrev = {
-    'weight': 'Weight',
-    'health': 'HP',
-    'eneCap': 'Energy',
-    'eneReg': 'Regeneration',
-    'heaCap': 'Heat',
-    'heaCol': 'Cooling',
-    'phyRes': 'Physical resistance',
-    'expRes': 'Explosive resistance',
-    'eleRes': 'Electric resistance',
-    'phyDmg': 'Damage',
-    'phyResDmg': 'Resistance drain',
-    'expDmg': 'Damage',
-    'heaDmg': 'Heat damage',
-    'heaCapDmg': 'Heat capacity drain',
-    'heaColDmg': 'Cooling damage',
-    'expResDmg': 'Resistance drain',
-    'eleDmg': 'Damage',
-    'eneDmg': 'Energy drain',
-    'eneCapDmg': 'Energy capacity drain',
-    'eneRegDmg': 'Regeneration damage',
-    'eleResDmg': 'Resistance drain',
-    'range': 'Range',
-    'push': 'Knockback',
-    'pull': 'Pull',
-    'recoil': 'Recoil',
-    'retreat': 'Retreat',
-    'advance': 'Advance',
-    'walk': 'Walking',
-    'jump': 'Jumping',
-    'uses': 'Use(s)',
-    'backfire': 'Backfire',
-    'heaCost': 'Heat cost',
-    'eneCost': 'Energy cost'}
-
-item_dict = {
-    'efa': 'Energy Free Armor',
-    'pp': 'Platinum Plating',
-    'vs': 'Valiant Sniper',
-    'rr': 'Rock Recoiler',
-    'mb': 'Magma Blast',
-    'recko': 'Reckoning',
-    'abo': 'Abomination',
-    'heron': 'HeronMark',
-    'mpv': 'Molten Platinum Vest',
-    'lpv': 'Lightning Platinum Vest',
-    'hpv': 'Hardened Platinum Vest',
-    'hsa': 'Hollow Spectral Armor',
-    'fha': 'Fractured Heat Armor',
-    'rea': 'Rusty Energy Armor',
-    'claw': 'The Claw',
-    'zark': 'Zarkares',
-    'upc': 'Unstable Power Cell'}
-
 WU_DB = {
     'trans_range': ['⚪', '🔵', '🟣', '🟠', '🟤', '⚪'],
     'tiers': ['COMMON', 'RARE', 'EPIC', 'LEGENDARY', 'MYTHICAL', 'DIVINE'],
