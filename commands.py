8f82a5985c4461c4632146c4a2cd784c13172156
Enegg <42005170+Enegg@users.noreply.github.com>
2020-06-15 21:34:08 +0200

    Moved "stats" command to commands module

--- commands.py
+++ commands.py
@@ -3,8 +3,10 @@ import asyncio
 import re
 import discord
 import socket
+import json
+import requests
 from random import randint
-from config import prefix_host, prefix_local, purge_confirm_emote, purge_cap, authorid, abbreviations
+from config import prefix_host, prefix_local, purge_confirm_emote, purge_cap, authorid, abbreviations, WU_DB
 client = discord.Client()
 prefix = prefix_host
 
@@ -54,7 +56,7 @@ def roll(a=1, b=6):
 
 #handlers
 def prefix_handler(msg, sep=' '):
-    txt = msg.content.replace(prefix, '')
+    txt = msg.content.replace(prefix, '', 1)
     cmd = re.match(r'(^[^\s]+)', txt)[0]
     txt = txt.replace(cmd, '')
     txt = re.sub(r'(^\s+|\s+(?=\s)|\s+$)', '', txt)
@@ -124,7 +126,6 @@ operation = {'add': ['health'],
     'reduce': ['backfire']}
 
 def buff(stat, value, enabled):
-    #print(stat, value, enabled, sep=', ')
     if enabled == False: return value
     if stat not in operation['add'] and stat not in operation['mult'] and stat not in operation['mult+'] and stat not in operation['reduce']: return value
     if stat in operation['add']: result = value + 350
@@ -132,3 +133,79 @@ def buff(stat, value, enabled):
     if stat in operation['mult+']: result = round(value * 1.4)
     if stat in operation['reduce']: result = round(value * 0.8)
     return result
+
+async def stats(message):
+    args = prefix_handler(message)[1]
+    if args[0] == '':
+        await message.channel.send('Funny command huh')
+        return
+    #flags
+    flags = []
+    for i in ['-d', '-b', '-r']:
+        if i in args:
+            flags.append(args.pop(args.index(i)))
+    buffs = '-b' in flags
+    #solving for abbrevs
+    name = ' '.join(args).lower()
+    if name in WU_DB['item_dict']:
+        name = WU_DB['item_dict'][name]
+    item = json.loads(requests.get(WU_DB['api_link'] + name).text)
+    #test flag
+    if '-r' in flags:
+        if permz(message) > 2:
+            await message.channel.send(item)
+            return
+        else:
+            await message.add_reaction('❌')
+            return
+    #error handling
+    if 'message' in item:
+        await message.add_reaction('❌')
+        if permz(message) > 3:
+            print(item)
+        return
+    #adding a note when -d or -b flag is included
+    note = ''
+    divine = False
+    beef = ', buffed' if buffs else ''
+    if '-d' in flags:
+        divine = 'divine' in item
+        note = ' (divine{})'.format(beef) if divine else ' (~~divine~~{})'.format(beef)
+    elif buffs == True:
+        note = ' (buffed)'
+    #adding item stats
+    item_stats = ''
+    spaced = False
+    for k in item['stats']:
+        if k in ['backfire', 'heaCost', 'eneCost'] and spaced == False:
+            item_stats += '\n'
+            spaced = True
+        y = 'stats'
+        #divine handler
+        if divine == True:
+            if k in item['divine']:
+                y = 'divine'
+        #number range handler
+        if isinstance(item['stats'][k], list):
+            #handling one spot range
+            if len(item['stats'][k]) == 1:
+                value = buff(k, item[y][k][0], buffs)
+            elif item[y][k][1] == 0:
+                value = item[y][k][0]
+            else:
+                value = str(buff(k, item[y][k][0], buffs)) + '-' + str(buff(k, item[y][k][1], buffs))
+        else:
+            value = buff(k, item[y][k], buffs)
+        item_stats = item_stats + '**{}**'.format(value) + ' {}\n'.format(WU_DB['WUabbrev'][k])
+    #embedding
+    embed = discord.Embed(title=item['name'], description=' '.join([item['element'].lower().capitalize(), item['type'].replace('_', ' ').lower()]), color=WU_DB['colors'][item['element']])
+    #transform range
+    min, max = item['transform_range'].split('-')
+    fields = []
+    fields.append(['Transform range: ', '{}'.format(''.join(WU_DB['trans_range'][WU_DB['tiers'].index(min):WU_DB['tiers'].index(max) + 1]))])
+    fields.append(['Stats{}:'.format(note), item_stats])
+    for field in fields:
+        embed.add_field(name=field[0], value=field[1], inline=False)
+    img_url = WU_DB['sprite_path'] + item['name'].replace(' ', '') + '.png'
+    embed.set_image(url=img_url)
+    await message.channel.send(embed=embed)
