df01d5358fdbfb59318847fa55ad95f5c67ebdc9
Enegg <42005170+Enegg@users.noreply.github.com>
2020-11-04 16:47:46 +0100

    more changes

--- Commands/Math.py
+++ Commands/Math.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from matrices import Matrix
+import asyncio
 
 class Math(commands.Cog):
     def __init__(self, bot):
@@ -10,19 +11,26 @@ class Math(commands.Cog):
     async def create_matrix(self, ctx):
         embed = discord.Embed(title='Matrix manipulation')
         embed.add_field(name='Starting', value='To begin, create a matrix\n```"new" (A-Z) YxX elements...```')
+        embed.set_footer(text='Type "exit" to exit')
         bot_msg = await ctx.send(embed=embed)
         exiting = False
         while True:
-            msg = await ctx.bot.wait_for('message', check=lambda m: m.channel == ctx.channel and m.author == ctx.author)
+            try:
+                msg = await ctx.bot.wait_for('message', timeout=120.0, check=lambda m: m.channel == ctx.channel and m.author == ctx.author)
+            except asyncio.TimeoutError:
+                await bot_msg.add_reaction('⏰')
+                break
             content = msg.content.split(' ')
             if exiting:
-                if content and content[0].lower() == 'yes': break
+                if content and content[0].lower() == 'yes':
+                    await bot_msg.edit(embed=embed.set_footer(text='Session has expired'))
+                    break
                 else:
                     exiting = False
                     embed.remove_field(-1)
                     await bot_msg.edit(embed=embed)
                     continue
-            if (exiting := (len(content) == 0)):
+            if (exiting := ('exit' in content)):
                 embed.add_field(name='Do you wish to exit?', value='[yes/no]', inline=False)
                 await bot_msg.edit(embed=embed)
                 continue
