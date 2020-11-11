b125988d37becded67968b36ea72f8fbee5620b3
Enegg <42005170+Enegg@users.noreply.github.com>
2020-11-11 20:28:58 +0100

    still unsolved, pushing to move workspace

--- Commands/Math.py
+++ Commands/Math.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
-from matrices import Matrix
 import asyncio
+import importlib
+Matrix = importlib.import_module('..matrices', 'Python_bot').Matrix
 
 class Math(commands.Cog):
     def __init__(self, bot):
@@ -14,11 +15,12 @@ class Math(commands.Cog):
         embed.set_footer(text='Type "exit" to exit')
         bot_msg = await ctx.send(embed=embed)
         exiting = False
+        matrices = {}
         while True:
             try:
                 msg = await ctx.bot.wait_for('message', timeout=120.0, check=lambda m: m.channel == ctx.channel and m.author == ctx.author)
             except asyncio.TimeoutError:
-                await bot_msg.add_reaction('⏰')
+                await bot_msg.edit(embed=embed.set_footer(text='Session has expired'))
                 break
             content = msg.content.split(' ')
             if exiting:
@@ -34,6 +36,26 @@ class Math(commands.Cog):
                 embed.add_field(name='Do you wish to exit?', value='[yes/no]', inline=False)
                 await bot_msg.edit(embed=embed)
                 continue
+            print(content)
+            if 'new' in content:
+                bad_input = ctx.send('Incorrect input format.', delete_after=5.0)
+                if len(content) < 4 or 'x' not in content[2] or len(content[1]) != 1:
+                    print(len(content) < 4, 'x' not in content[2], len(content[1]) != 1)
+                    await bad_input
+                    continue
+                size = tuple(int(x) for x in content[2].split('x'))
+                try:
+                    values = [int(n) for n in content[3:]]
+                except ValueError:
+                    print('error')
+                    await bad_input
+                    continue
+                if len(values) != size[0] * size[1]:
+                    await ctx.send('Incorrect amount of values for the matrix.', delete_after=5.0)
+                matrices[content[1]] = Matrix(*size, values)
+                embed.insert_field_at(0, name='Matrices', value=f'{matrices}', inline=False)
+                print(embed.fields[0])
+                bot_msg.edit(embed=embed)
         pass
 
 def setup(bot):
