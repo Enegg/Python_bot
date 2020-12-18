adc773aa421073c96a1ea63627693457e07324cc
Enegg <42005170+Enegg@users.noreply.github.com>
2020-12-18 01:03:13 +0100

    userinfo can now fetch users

--- Commands/Moderation.py
+++ Commands/Moderation.py
@@ -1,5 +1,6 @@
 import asyncio
 import datetime
+import typing
 
 import discord
 from discord.ext import commands
@@ -12,17 +13,16 @@ class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(aliases=['wot','ins'],brief='Helper command used to retrieve data about the guild')
+    @commands.group(aliases=['wot','ins', 'info'], brief='Helper command used to retrieve data about the guild')
     @perms(2)
     async def inspect(self, ctx: commands.Context):
         if ctx.invoked_subcommand is None:
             guild = ctx.guild
-            text = (
-                f'**Text channels**: {len(guild.text_channels)}\n'
-                f'**Members**: {guild.member_count}\n'
-                f'**Created at**: {guild.created_at.date()} ({(datetime.datetime.today() - guild.created_at).days} days ago)')
-            embed = discord.Embed(title=guild.name, description=f'**Owner:** {guild.owner.mention}', color=guild.owner.color)
-            embed.add_field(name='Statistics:', value=text, inline=False)
+            embed = discord.Embed(title=guild.name, color=guild.owner.color, timestamp=guild.created_at)
+            embed.add_field(name='Owner', value=f'{guild.owner.mention}')
+            embed.add_field(name='Text channels', value=f'{len(guild.text_channels)}')
+            embed.add_field(name='Members', value=f'{guild.member_count}')
+            embed.set_footer(text=f'Created {(datetime.datetime.today() - guild.created_at).days} days ago')
             embed.set_thumbnail(url=guild.icon_url)
             await ctx.send(embed=embed)
 
@@ -31,38 +31,59 @@ class Moderation(commands.Cog):
         result = '\n'.join(f'<#{x.id}>: {x.id}' for x in ctx.guild.channels if isinstance(x, discord.TextChannel))
         await ctx.send(result)
 
-    @inspect.command(brief='Returns info about the invoker or pinged member (you can use his ID)')
-    async def user(self, ctx: commands.Context, *args):
-        mentions = ctx.message.mentions
-        mentioned = bool(mentions)
-        provided_args = bool(args)
-        if mentioned: member = mentions[0]
-        elif not mentioned and not provided_args:
+    @commands.command(aliases=['user'], brief='Returns info about the invoker or pinged member (you can use his ID)')
+    @perms(2)
+    async def userinfo(self, ctx: commands.Context, member: typing.Union[discord.Member, int]=None):
+        if member is None:
             member = ctx.author
-        elif provided_args:
-            member = ctx.guild.get_member(intify(args[0]))
-            if member is None:
+            is_fetched = False
+        elif is_fetched := isinstance(member, int):
+            if len(str(member)) != 18:
                 await ctx.send('Invalid ID.')
                 return
-        important = ['administrator', 'manage_guild', 'manage_channels', 'manage_messages', 'manage_roles']
-        if member.guild_permissions.administrator:
-            high_rank = 'Server owner' if member.id == getattr(ctx.guild.owner, 'id', None) else 'Administrator'
-        else:
-            high_rank = ''
-            privs = filter(lambda perm: perm[1] and perm[0] in important, dict(member.guild_permissions).items())
-        notable = high_rank or '\n'.join(x[0].capitalize() for x in privs).replace('_', ' ') or 'None'
-        creation_date = f'**Account created at**: {member.created_at.date()} ({(datetime.datetime.today() - member.created_at).days} days ago)'
-        join_date = f'**Joined at**: {member.joined_at.date()} ({(datetime.datetime.today() - member.joined_at).days} days ago)'
-        roles = ', '.join(x.mention for x in reversed(member.roles) if x.name != '@everyone')
-        data = f'**User ID**: {member.id}\n{creation_date}\n{join_date}\n**Roles**: {roles}\n**Notable privileges**:\n{notable}'
-        embed = discord.Embed(title=f'{member.name}{f" ({member.nick})" if member.nick else ""}', description=data, color=member.color)
+            botmsg = await ctx.send('User not found. Attempt fetching (API call)?')
+            check = lambda m: m.channel == ctx.channel and m.author == ctx.author
+            try:
+                response = await ctx.bot.wait_for('message', timeout=10.0, check=check)
+            except asyncio.TimeoutError:
+                await botmsg.delete()
+                return
+            if response.content.lower() in 'yes':
+                try:
+                    member = await ctx.bot.fetch_user(member)
+                except discord.NotFound:
+                    await ctx.send("Couldn't find the user.")
+                    return
+
+        created_days = (datetime.datetime.today() - member.created_at).days
+        data = (
+            f'**User**: {member.mention}'
+            f'\n**ID**: {member.id}'
+            f'\n**Account created at**: {member.created_at.strftime(r"%d/%m/%Y")} ({created_days} days ago)')
+
+        if not is_fetched:
+            important = {'administrator', 'manage_guild', 'manage_channels', 'manage_messages', 'manage_roles'}
+            if member.guild_permissions.administrator:
+                high_rank = 'Server owner' if member.id == getattr(ctx.guild.owner, 'id', None) else 'Administrator'
+            else:
+                high_rank = ''
+                privs = filter(lambda perm: perm[1] and perm[0] in important, member.guild_permissions)
+            notable = high_rank or '\n'.join(x[0].title() for x in privs).replace('_', ' ') or 'Basic'
+            join_days = (datetime.datetime.today() - member.joined_at).days
+            roles = ', '.join(x.mention for x in reversed(member.roles[1:]))
+            data += (
+                f'\n**Joined at**: {member.joined_at.strftime(r"%d/%m/%Y")} ({join_days} days ago)'
+                f'\n**Roles**: {roles}'
+                f'\n**Notable privileges**:\n{notable}')
+        embed = discord.Embed(title=f'{member.name}{f" ({member.nick})" if not is_fetched and member.nick else ""}',
+            description=data, color=member.color)
         embed.set_thumbnail(url=member.avatar_url)
         await ctx.send(embed=embed)
 
     @commands.command(brief='Returns a dict of your roles')
     @perms(1)
     async def roles(self, ctx: commands.Context):
-        await ctx.send('`{}`'.format(ctx.author.roles))
+        await ctx.send(f'`{ctx.author.roles}`')
 
     @commands.command(
         aliases=['prune','pirge','puerg','p'],
@@ -71,21 +92,25 @@ class Moderation(commands.Cog):
         help=('Deletes a specified number of messages from the channel it has been used in.'
               ' You can specify whose messages to purge by pinging one or more users'))
     @perms(1)
-    async def purge(self, ctx: commands.Context, *args):
+    async def purge(self, ctx: commands.Context, count: typing.Optional[int], members: commands.Greedy[discord.Member]):
         msg = ctx.message
-        mentions = msg.mentions if (has_mentions := bool(msg.mentions)) else None
-        def purger(n): return ctx.channel.purge(limit=n, check=lambda m: not m.pinned and (m.author in mentions if has_mentions else True))
+        def purger(n):
+            check = lambda m: not m.pinned and (not members or m.author in members)
+            return ctx.channel.purge(limit=n, check=check)
         await msg.delete()
-        count = intify(args[0]) if args else 1
+        if count is None:
+            count = 1
         if count > purge_cap:
             count = purge_cap
         if count <= 10: 
             await purger(count)
         else:
-            botmsg = await ctx.send(f"You are going to purge {'messages of {} in the recent '.format(', '.join(x.name for x in mentions)) if has_mentions else ''}{count} messages, continue?")
+            victims = f"messages of {', '.join(x.name for x in members)} in the recent " if members else ''
+            botmsg = await ctx.send(f'You are going to purge {victims}{count} messages, continue?')
             await botmsg.add_reaction(purge_confirm_emote)
+            check = lambda reaction, user: user == ctx.author and str(reaction.emoji) == purge_confirm_emote
             try: 
-                await ctx.bot.wait_for('reaction_add', timeout=20.0, check=lambda reaction, user: user == ctx.author and str(reaction.emoji) == purge_confirm_emote)
+                await ctx.bot.wait_for('reaction_add', timeout=20.0, check=check)
             except asyncio.TimeoutError:
                 await botmsg.delete()
             else:
